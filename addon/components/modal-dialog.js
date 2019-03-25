fbc55b58598c3a2698355d1e042a3be99ac2ef06
Bill Dami <bdami@gavant.com>
2019-03-25 13:28:05 -0400

    remove jquery usage to prep for ember octane

--- addon/components/modal-dialog.js
+++ addon/components/modal-dialog.js
@@ -1,10 +1,9 @@
 import ModalDialog from 'ember-modal-dialog/components/modal-dialog';
 import layout from '../templates/components/modal-dialog';
 import { inject as service } from '@ember/service';
-import { get, computed } from '@ember/object';
+import { get, computed, setProperties } from '@ember/object';
 import { tryInvoke } from '@ember/utils';
 import { later, bind } from '@ember/runloop';
-import $ from 'jquery';
 
 const ESC_KEY = 27;
 
@@ -27,25 +26,29 @@ export default ModalDialog.extend({
 
     didInsertElement() {
         this._super(...arguments);
-        $(document).on('keyup.modal-dialog', bind(this, '_escapeListener'));
-        $(document).on('click.modal-dialog', '.modal', bind(this, '_modalClickListener'));
-        $('body').addClass('modal-open');
+        const keyupHandler = bind(this, 'onDocumentKeyup');
+        const clickHandler = bind(this, 'onDocumentClick');
+        document.addEventListener('keyup', keyupHandler);
+        document.addEventListener('click', clickHandler);
+        document.body.classList.add('modal-open');
+        setProperties(this, {keyupHandler, clickHandler});
     },
     willDestroyElement() {
         this._super(...arguments);
-        $(document).off('.modal-dialog');
-        later($('body'), 'removeClass', 'modal-open', get(this, 'modal.animationDuration'));
+        document.removeEventListener('keyup', get(this, 'keyupHandler'));
+        document.removeEventListener('click', get(this, 'clickHandler'));
+        later(document.body.classList, 'remove', 'modal-open', get(this, 'modal.animationDuration'));
     },
-    _escapeListener(event) {
+    onDocumentKeyup(event) {
         if (event.keyCode === ESC_KEY && get(this, 'closable')) {
             tryInvoke(this, 'onClose');
         }
     },
-    _modalClickListener(event) {
+    onDocumentClick(event) {
         //clicks directly on the modal container should behave as clicks on the overlay backdrop
         //this is necessary because bootstrap's .modal container stretches to cover the entire viewport
         //and has a higher z-index ordering than the overlay backdrop
-        if($(event.target).is('.modal') && get(this, 'closable')) {
+        if(event.target.matches('.modal') && get(this, 'closable')) {
             this.actions.onClickOverlay.apply(this, [event]);
         }
     }
