ac3319ae7803ee7de44659bf1ca9635fe248aaed
Nick Pafundi <npafundi@gavant.com>
2020-02-21 09:57:30 -0500

    ran ember-cli-update

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,14 +1,11 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  extends: 'recommended',
-  rules: {
-    'no-bare-strings': true,
-    'block-indentation': 4,
-    'attribute-indentation': false
-  },
-  ignore: [
-    'dummy/templates/**',
-    'addon/templates/**'
-  ]
+    extends: "octane",
+    rules: {
+        "no-bare-strings": true,
+        "block-indentation": 4,
+        "attribute-indentation": false
+    },
+    ignore: ["dummy/templates/**", "addon/templates/**"]
 };
--- README.md
+++ README.md
@@ -7,8 +7,9 @@ DISCLAIMER: This addon is not actively maintained for public use. Pull requests 
 Compatibility
 ------------------------------------------------------------------------------
 
-* Ember.js v2.18 or above
+* Ember.js v3.12 or above
 * Ember CLI v2.13 or above
+* Node.js v10 or above
 
 
 Installation
