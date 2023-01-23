5d4aaeacb78415b2b6895dda29c887cce944b641
Adam Baker <bakerac4@gmail.com>
2023-01-23 11:44:47 -0500

    Upgrading glint deps

--- ember-modals/src/components/modal/body/index.ts
+++ ember-modals/src/components/modal/body/index.ts
@@ -1,4 +1,9 @@
 import Component from '@glimmer/component';
 
+interface ModalDialogBodySignature {
+    Blocks: {
+        default: [];
+    };
+}
 // eslint-disable-next-line ember/no-empty-glimmer-component-classes
-export default class ModalDialogBody extends Component {}
+export default class ModalDialogBody extends Component<ModalDialogBodySignature> {}
